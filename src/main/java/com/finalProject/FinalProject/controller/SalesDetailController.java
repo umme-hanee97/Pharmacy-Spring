361907a83c9f762a71828f918ca4240eb51adbde
Umme Hanee <73064213+umme-hanee97@users.noreply.github.com>
2026-03-06 00:35:33 +0600

    Update CORS origin for SalesDetailController

--- src/main/java/com/finalProject/FinalProject/controller/SalesDetailController.java
+++ src/main/java/com/finalProject/FinalProject/controller/SalesDetailController.java
@@ -24,7 +24,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/saledet")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@CrossOrigin(origins = "https://pharmacy-angular.onrender.com/", allowCredentials = "true")
 public class SalesDetailController {
 
     @Autowired
