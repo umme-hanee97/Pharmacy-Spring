4f841598da79ea84981672d4d65bac30d123ff98
Umme Hanee <73064213+umme-hanee97@users.noreply.github.com>
2026-03-06 00:28:14 +0600

    Update CORS configuration for CompanyController

--- src/main/java/com/finalProject/FinalProject/controller/CompanyController.java
+++ src/main/java/com/finalProject/FinalProject/controller/CompanyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/company")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@CrossOrigin(origins = "https://pharmacy-angular.onrender.com/", allowCredentials = "true")
 public class CompanyController {
 
     @Autowired
