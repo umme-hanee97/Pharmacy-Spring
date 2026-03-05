d121dc2ae673300a3e492fda3408a0527b96e156
Umme Hanee <73064213+umme-hanee97@users.noreply.github.com>
2026-03-06 00:32:46 +0600

    Update CORS configuration for ReturnedProductShopController

--- src/main/java/com/finalProject/FinalProject/controller/ReturnedProductShopController.java
+++ src/main/java/com/finalProject/FinalProject/controller/ReturnedProductShopController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/retpro")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@CrossOrigin(origins = "https://pharmacy-angular.onrender.com/", allowCredentials = "true")
 public class ReturnedProductShopController {
 
     @Autowired
